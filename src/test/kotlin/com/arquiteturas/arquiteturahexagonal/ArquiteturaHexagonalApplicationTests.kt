9c5dae9c715bf5cc14e62b468d8c75798f82149d
Otávio de Castro <otavio.vn01@hotmail.com>
2024-01-09 13:54:25 -0300

    teste unitário

---
+++ src/test/kotlin/com/arquiteturas/arquiteturahexagonal/ArquiteturaHexagonalApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.arquiteturas.arquiteturahexagonal
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ArquiteturaHexagonalApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
