7a8b920370105f6fadcf777877c6e71eb9cb3f1b
Otávio de Castro <otavio.vn01@hotmail.com>
2024-01-09 13:43:19 -0300

    commit inicial

---
+++ src/main/kotlin/com/arquiteturas/arquiteturahexagonal/ArquiteturaHexagonalApplication.kt
@@ -0,0 +1,11 @@
+package com.arquiteturas.arquiteturahexagonal
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ArquiteturaHexagonalApplication
+
+fun main(args: Array<String>) {
+	runApplication<ArquiteturaHexagonalApplication>(*args)
+}
